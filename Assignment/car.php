d0532affd70d48e22ee2f8438637321c850437c2
KToan Nguyen <nguyenkhanhtoan7b6@gmail.com>
2026-01-19 23:53:48 -0500

    Car.php created

---
+++ Assignment/car.php
@@ -0,0 +1,26 @@
+<?php
+//make PHP strict
+declare(strict_types=1); 
+require_once "connect.php"; //include "connect.php"
+
+//Creating a class show the info of the car
+class Car{
+    public string $brand; //Car's brand
+    public string $model; //Car's model
+    public int $year; //Model's release year
+
+    public function __construct(string $brand, string $model, int $year)
+    {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->year = $year;
+    }
+
+    public function getBadge(): string{
+        return "Brand: {$this->brand} | Model: {$this->model} | Year: {$this->year}";
+    }
+}
+
+//Creating an instance from the above class
+$car = new Car("Koenigsegg", "Jesko Absolut", 2020);
+echo $car->getBadge();
